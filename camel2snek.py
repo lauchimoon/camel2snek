2b78cf758e89952955cb99962673b5a1c4418eaa
lauchimoon <laurentino.luna06@gmail.com>
2026-06-30 15:24:25 -0300

    Add support for files

--- camel2snek.py
+++ camel2snek.py
@@ -3,7 +3,7 @@ from sys import argv
 PROGRAM_NAME = "camel2snek.py"
 
 def usage():
-    print(f"usage: ./{PROGRAM_NAME} <text>")
+    print(f"usage: python3 {PROGRAM_NAME} <text>|--file <filename>")
     exit(0)
 
 def camel2snek(text: str) -> str:
@@ -15,12 +15,30 @@ def camel2snek(text: str) -> str:
 
     return new_text
 
+def camel2snek_file(filename: str) -> str:
+    try:
+        f = open(filename, 'r')
+    except FileNotFoundError:
+        print(f"{PROGRAM_NAME}: no such file '{filename}' found")
+
+    text = ""
+    for line in f:
+        text += camel2snek(line)
+
+    return text
+
 def main():
     argc = len(argv)
     if argc < 2:
         usage()
 
-    print(camel2snek(argv[1]))
+    if argv[1] == "--file":
+        if argc < 3:
+            usage()
+
+        print(camel2snek_file(argv[2]))
+    else:
+        print(camel2snek(argv[1]))
 
 if __name__ == "__main__":
     main()
